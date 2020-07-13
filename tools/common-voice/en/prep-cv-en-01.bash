4e09c733ad5083d47484282f54d3705e3560bcc2
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-07-13 22:37:15 +0000

    prep-cv-en-01.bash: extract_path -> extracted_path

--- tools/common-voice/en/prep-cv-en-01.bash
+++ tools/common-voice/en/prep-cv-en-01.bash
@@ -42,13 +42,13 @@ wget -cO "$download_path" "$download_url"
 echo
 echo "Extracting:"
 
-extract_path="$cv_path/extracted"
+extracted_path="$cv_path/extracted"
 
-if [[ ! -e "$extract_path" ]]; then
-    mkdir -p "$extract_path"
-    tar -xvzf "$download_path" -C "$extract_path"
+if [[ ! -e "$extracted_path" ]]; then
+    mkdir -p "$extracted_path"
+    tar -xvzf "$download_path" -C "$extracted_path"
 else
-    echo "Path $extract_path exists"
+    echo "Path $extracted_path exists"
 fi
 
 ####################
@@ -60,7 +60,7 @@ channels=1
 format=pcm_s16le
 
 tsv_names=( "dev.tsv" "test.tsv" "train.tsv" )
-mp3_path="$extract_path/clips"
+mp3_path="$extracted_path/clips"
 
 audio_path="$cv_path/audio"
 
@@ -68,7 +68,7 @@ if [[ ! -e "$audio_path" ]]; then
     mkdir -p "$audio_path"
 
     for tsv_name in ${tsv_names[*]}; do
-        tsv_path="$extract_path/$tsv_name"
+        tsv_path="$extracted_path/$tsv_name"
         echo "Processing: $tsv_path"
 
         cat "$tsv_path" | cut -d$'\t' -f2 | cut -d'.' -f1 | tail -n+2 | xargs -P`nproc` -I{} -n1 \
