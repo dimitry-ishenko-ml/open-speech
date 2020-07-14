d06f8b064a6d24d133295012d47e477524758823
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-07-14 15:59:45 +0000

    prep-vox-en-02.bash: update

--- tools/voxforge/en/prep-vox-en-02.bash
+++ tools/voxforge/en/prep-vox-en-02.bash
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# This script will extract all archive files located in `files_path` into the
-# `extracted_path` directory.
+# Extract all archives into the `extracted_path` directory.
 
 ####################
-echo
-echo "Extracting files:"
-
 vox_path="$HOME/tensorflow_datasets/manual/voxforge/en"
 files_path="$vox_path/files"
 
 extracted_path="$vox_path/extracted"
 mkdir -p "$extracted_path"
 
-find "$files_path" -name "*.tgz" | sed -nr "s|^.*/(.*)\.tgz$|\1|p" | sort | \
-    xargs -P`nproc` -I{} -n1 sh -c "echo '  {}'; tar -xzf '$files_path/{}.tgz' -C '$extracted_path'"
+echo "Extracting:"
+
+find "$files_path" -name "*.tgz" | grep -o "[^/]*$" | sort | \
+    xargs -P`nproc` -I{} -n1 sh -c "echo '  {}'; tar -xzf '$files_path/{}' -C '$extracted_path'"
