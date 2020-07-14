2a4d10f9c4fccaa6be3c9a23e05170f5b87029fd
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-07-14 15:31:43 +0000

    prep-vox-en-01.bash: update

--- tools/voxforge/en/prep-vox-en-01.bash
+++ tools/voxforge/en/prep-vox-en-01.bash
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# This script will download [VoxForge](http://www.voxforge.org) dataset index
-# and archive files into `vox_path` and `files_path` directories respectively.
+# Download [VoxForge](http://www.voxforge.org) dataset index and archives into
+# the `files_path` directory.
 
 ####################
-echo
-echo "Downloading index:"
+download_url="http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit"
 
 vox_path="$HOME/tensorflow_datasets/manual/voxforge/en"
-mkdir -p "$vox_path"
-
-download_url="http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit"
-index_path="$vox_path/index"
-
-echo "$download_url"
-wget -qcO- "$download_url" | sed -nr 's/^.*href="([^"]*\.tgz)".*$/\1/p' > "$index_path"
-
-echo
-echo "Downloading files:"
 
 files_path="$vox_path/files"
 mkdir -p "$files_path"
-max_dl=16
 
+index_path="$files_path/index"
+
+echo "Downloading index:"
+echo "$download_url"
+
+wget -qcO- "$download_url" | sed -nr 's/^.*href="([^"]*\.tgz)".*$/\1/p' > "$index_path"
+echo
+
+echo "Downloading file:"
+
+max_dl=16
 cat "$index_path" | xargs -P$max_dl -I{} -n1 \
     sh -c "echo '  {}'; wget -qcO '$files_path/{}' '$download_url/{}'"
