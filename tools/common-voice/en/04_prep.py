273a2ac9ac1cc4719d5cf8232a52ec7ed73b5a50
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-07-18 19:14:06 +0000

    prep-cv-en-02.py -> 04_prep.py
    Discard files over 20s long.

--- tools/common-voice/en/04_prep.py
+++ tools/common-voice/en/04_prep.py
@@ -18,21 +18,25 @@ from unidecode import unidecode
 cv_path = "~/tensorflow_datasets/manual/common-voice/en"
 cv_path = Path(cv_path).expanduser()
 
-extracted_path = cv_path / "extracted"
+extracted_path = cv_path / "extracted" / "cv-corpus-5.1-2020-06-22" / "en"
 tsv_names = [ "dev.tsv", "test.tsv", "train.tsv" ]
 
 audio_path = cv_path / "audio"
 
 remove_table = str.maketrans("", "", "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~")
 
+max_size = int(20 * 16000 * 2 * 1.05) # 20s @ 16kHz, 16-bit + 5%
+
 def clean(sentence):
     sentence = unidecode(sentence)
+    sentence = sentence.strip()
     sentence = sentence.lower()
     sentence = sentence.translate(remove_table)
     return sentence
 
 def read_data(tsv_path):
     rows = []
+    errors = []
     with open(tsv_path) as file:
         for row in csv.DictReader(file, delimiter="\t"):
 
@@ -42,23 +46,35 @@ def read_data(tsv_path):
 
             path = audio_path / name
             if not path.exists():
-                print("Missing file:", path)
+                errors.append("Missing file: " + str(path))
 
-            else: rows.append({
-                "path"    : str(name),
-                "sentence": sentence,
-                "original": original,
-                "size"    : path.stat().st_size
-            })
-    return pd.DataFrame(rows)
+            else:
+                size = path.stat().st_size
+                if size > max_size:
+                    errors.append("Long file: " + str(path))
+
+                else: rows.append({
+                    "path"    : str(name),
+                    "size"    : size,
+                    "sentence": sentence,
+                    "original": original,
+                })
+
+    return rows, errors
 
 for tsv_name in tsv_names:
     tsv_path = extracted_path / tsv_name
     json_path = (audio_path / tsv_name).with_suffix(".json")
 
     print("\nProccessing:", tsv_path)
-    data = read_data(tsv_path)
+    rows, errors = read_data(tsv_path)
+
+    data = pd.DataFrame(rows)
     print("Total examples:", len(data))
 
     print("Saving to:", json_path)
     data.to_json(json_path, orient="table")
+
+    if len(errors):
+        print("\nErrors:")
+        for error in errors: print(error)
