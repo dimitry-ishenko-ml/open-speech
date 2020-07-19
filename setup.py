195da2d9dc89f396d9ef7830a3ba642d7fecbca0
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2020-07-19 02:33:39 +0000

    version 3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as file:
     long_description = file.read()
 
 setuptools.setup(name="open_speech",
-    version="2.0",
+    version="3.0",
     author="Dimitry Ishenko",
     author_email="dimitry.ishenko@gmail.com",
     description="Open Speech Datasets",
